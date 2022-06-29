75820288bcbef2dcfa31edf88117b8d84d08b902
Theodore Ayoub <theodore.ayoub@gmail.com>
2022-06-29 11:11:07 -0700

    coded associations

--- models/index.js
+++ models/index.js
@@ -4,13 +4,25 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Product belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+// Category have many Products
+// if a Category is deleted then the Product associated
+// should be deleted
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
-// Categories have many Products
+
+// https://sequelize.org/docs/v6/core-concepts/assocs/
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, { through: ProductTag });
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag });
 
 module.exports = {
   Product,
